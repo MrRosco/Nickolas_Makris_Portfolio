f0cff2accdcb565eb2fde1dc12d67441e0970639
Nikos Makris <nikosmak64@gmail.com>
2026-07-14 17:57:17 +0300

    -

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@
 
 function test(){
     document.querySelector(".div1").innerHTML = "Hello!";
+    document.querySelector(".div1").innerHTML = "My name is Nickolas and this site is currently under construction!";
 }
 
 test();
